cd84656a0d036a56ee45fad5b8d25637b358613c
TAMIT001 <phanminhtam247@gmail.com>
2018-03-04 21:57:46 +0700

    add slide

---
+++ Assignment/MergeSortQuickSort/src/Merge.java
@@ -0,0 +1,31 @@
+/**
+ * Created by MinhTam on  2/28/2018.
+ */
+public class Merge {
+    /**
+     *
+     Question 1
+     Merging with smaller auxiliary array. Suppose that the subarray
+     a[0] to a[n−1] is sorted and the subarray
+     a[n] to a[2∗n−1]is sorted. How can you merge the two subarrays so that  a[0]
+     to a[2∗n−1]is sorted using an auxiliary array of length n
+     (instead of 2n)
+     coppy a[0] to a[n - 1] to auxiliary array after merge vs a[n] to a[2*n -1]
+     * @param argv
+     */
+
+    /**
+     * Counting inversions. An inversion in an array
+     a[] is a pair of entries a[i]and a[j] such that i<j but a[i]>a[j]
+     Given an array, design a linearithmic algorithm to count the
+     number of inversions.
+
+     count while mergesorting.
+     * @param argv
+     */
+
+
+    public static void main(String[] argv) {
+        System.out.println("hello world");
+    }
+}
